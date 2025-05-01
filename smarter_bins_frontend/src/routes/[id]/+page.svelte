1efc26dd73268792376f0de5781e93b0713389df
Gabriel Finke <finkegabriel@gmail.com>
2025-05-01 20:21:47 +0000

    dynamic [id] matches with bin id

--- smarter_bins_frontend/src/routes/[id]/+page.svelte
+++ smarter_bins_frontend/src/routes/[id]/+page.svelte
@@ -1,7 +1,45 @@
 <script>
     import { page } from '$app/stores';
-
-    $: userId = $page.params.id;</script>
-
-<h1>Home</h1>
-<h1>Bin # {userId}</h1>
+    // Example data
+    let headers = ['id', 'part name', 'quantity'];
+    let rows = [
+      [ $page.params.id, "photo resistor sensor", 'x4'],
+      [ $page.params.id, "arduino nano", 'x3'],
+      [ $page.params.id, "stepper motor driver", 'x10']
+    ];
+  </script>
+  
+  <table>
+    <thead>
+      <tr>
+        {#each headers as header}
+          <th>{header}</th>
+        {/each}
+      </tr>
+    </thead>
+    <tbody>
+      {#each rows as row}
+        <tr>
+          {#each row as cell}
+            <td>{cell}</td>
+          {/each}
+        </tr>
+      {/each}
+    </tbody>
+  </table>
+  
+  <style>
+    table {
+      border-collapse: collapse;
+      width: 100%;
+    }
+    th, td {
+      border: 1px solid #ccc;
+      padding: 0.5rem;
+      text-align: left;
+    }
+    th {
+      background-color: #f4f4f4;
+    }
+  </style>
+  
