ac6994f954fce4172fe74ad06cc38d18a8329de8
finkegabriel <finkegabriel@gmail.com>
2025-05-24 15:45:35 -0700

    update login

--- smarter_bins_frontend/src/App.svelte
+++ smarter_bins_frontend/src/App.svelte
@@ -4,41 +4,42 @@
   import { Router, Link, Route } from "svelte-routing";
   import Home from "./routes/Home.svelte";
   import Search from "./routes/Search.svelte";
-  import { FirebaseApp } from 'sveltefire';
-  import { initializeApp } from 'firebase/app';
-  import { getFirestore } from 'firebase/firestore';
-  import { getAuth } from 'firebase/auth';
-  import Login from "./routes/auth/login/login.svelte";
+  // import { FirebaseApp } from 'sveltefire';
+  // import { initializeApp } from 'firebase/app';
+  // import { getFirestore } from 'firebase/firestore';
+  // import { getAuth } from 'firebase/auth';
+  // import Login from "./routes/login.svelte";
+  import DyanmicParts from './routes/[id]/+page.svelte';
 
     // Initialize Firebase
-    const app = initializeApp(/* your firebase config */);
-    const firestore = getFirestore(app);
-    const auth = getAuth(app);
+    // const app = initializeApp(/* your firebase config */);
+    // const firestore = getFirestore(app);
+    // const auth = getAuth(app);
   export let url = "";
 
 </script>
 
 
-<FirebaseApp {auth} {firestore}>
+<!-- <FirebaseApp {auth} {firestore}> -->
 <Router {url}>
   <main>
     <nav>
       <Link to="/">Home</Link>
       <Link to="/search">Search</Link>
-      <Link to="/auth/login">Login</Link>
+      <Link to="/:id">Dynamic Parts</Link>
     </nav>
 
     <div class="content">
-      <Route path="/search" component={Search}>
-      </Route>
-      <Route path="/auth/login" component={Login}>
-      </Route>
       <Route path="/" component={Home}>
+      </Route>
+      <Route path="/:id" component={DyanmicParts}>
+      </Route>
+      <Route path="/search" component={Search}>
       </Route>
     </div>
   </main>
 </Router>
-</FirebaseApp>
+<!-- </FirebaseApp> -->
 
 <style>
   main {
